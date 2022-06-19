70a502c204e80b6d39a07c694ab3c104a93f8182
RPatryk98 <99049638@mydavinci.nl>
2022-06-19 21:43:13 +0200

    7

--- Pokemon.php
+++ Pokemon.php
@@ -14,6 +14,27 @@ class pokemon {
 		$this->name = $name;
 		$this->health = $health;
 		$this->hitPoints = $hitPoints;
+		self::$pokemonAliveCounter++;
+	}
+
+	public function __toString() {
+		return json_encode($this);
+	}
+
+
+
+	public function attack($attack, $guardian) {
+
+		if($this->energyType == $guardian->weakness) {
+			$guardian->hitPoints -= $attack['blow'] * 1.5;
+		}
+		else if($this->energyType == $guardian->resistance) {
+			$guardian->hitPoints -= $attack['blow'] * 0.5;
+		}
+		else {
+			$guardian->hitPoints -= $attack['blow'];
+		}
+
 	}
 
 }
