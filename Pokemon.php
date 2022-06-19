1517bf1d6cf9ac512cf23768f2d9e1f933102d63
RPatryk98 <99049638@mydavinci.nl>
2022-06-19 21:30:07 +0200

    6

--- Pokemon.php
+++ Pokemon.php
@@ -5,9 +5,15 @@ class pokemon {
 	public $energyType;
 	public $hitPoints;
 	public $attacks;
+	public $health;
 	public $weakness;
 	public $resistance;
-	
+	public static $pokemonAliveCounter = 0;
 
+	public function __construct($name, $health, $hitPoints) {
+		$this->name = $name;
+		$this->health = $health;
+		$this->hitPoints = $hitPoints;
+	}
 
 }
