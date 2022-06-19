31a578086bc8282650f841eb40b04cd7f907a0d4
RPatryk98 <99049638@mydavinci.nl>
2022-06-19 21:49:26 +0200

    8

--- index.php
+++ index.php
@@ -2,3 +2,8 @@
 
 require 'pokemon.php';
 
+$pickachu = new pokemon('Pickahu', 60 60, [array("name" => "Electric Ring", "blow" => 50),
+array("name" => "Pika Punch", "blow" => 20)]);
+
+$charmander = new pokemon('Charmander', 60, 60, [array("name" => "Head Butt", "blow" => 10), 
+array("name" => "Flare", "blow" => 30)]);
